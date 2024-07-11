9301043b459767f126e3072a97a8bc5d051e53e6
mahesh jamdade <maheshmn121@gmail.com>
2024-07-11 00:23:24 -0400

    update skills layout

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -9,7 +9,7 @@ const SkillCard = ({ skill, Icon, color }: { skill: string, Icon: IconType, colo
 
     return (
         <div className="flex flex-col items-center p-4  bg-opacity-20 rounded-lg transition-shadow duration-300">
-            <Icon className={color} size={32} />
+            <Icon className={`${color} md:h-8 md:w-8 xs:h-4 xs:w-4`} />
             <span className={`${darkMode ? 'text-white' : 'text-black'} mt-2 text-lg font-semibold`}>{skill}</span>
         </div>
     );
@@ -17,18 +17,23 @@ const SkillCard = ({ skill, Icon, color }: { skill: string, Icon: IconType, colo
 
 function Skills() {
     const { darkMode } = useDarkMode();
-
-    const skills = [
-        { skill: "Typescript", icon: SiTypescript, color: 'text-green-600' },
+    const programmingLanguages = [
+        {
+            skill: "Typescript", icon: SiTypescript, color: 'text-green-600'
+        },
         { skill: "Python", icon: SiPython, color: 'text-blue-600' },
-        { skill: "Dart", icon: SiDart, color: 'text-blue-500' },
+        { skill: "Dart", icon: SiDart, color: 'text-blue-500' }
+    ];
+    const frameworks = [
         { skill: "React", icon: SiReact, color: 'text-blue-600' },
         { skill: "Node.js", icon: SiNodedotjs, color: 'text-green-600' },
-        { skill: "Git", icon: SiGit, color: 'text-red-600' },
-        { skill: "Flutter", icon: SiFlutter, color: 'text-blue-500' },
         { skill: "Android", icon: SiAndroid, color: 'text-green-600' },
-        { skill: "Docker", icon: SiDocker, color: 'text-blue-600' },
         { skill: "Tailwind CSS", icon: SiTailwindcss, color: 'text-blue-500' },
+        { skill: "Flutter", icon: SiFlutter, color: 'text-blue-500' },
+    ]
+    const tools = [
+        { skill: "Git", icon: SiGit, color: 'text-red-600' },
+        { skill: "Docker", icon: SiDocker, color: 'text-blue-600' },
         { skill: "PostgreSQL", icon: SiPostgresql, color: 'text-blue-600' },
         { skill: "MongoDB", icon: SiMongodb, color: 'text-green-600' },
         // { skill: "HTML5", icon: SiHtml5, color: 'orange' },
@@ -41,8 +46,18 @@ function Skills() {
         <section className={`py-16 text-gray-800`}>
             <div className="container mx-auto px-4">
                 <h2 className={`text-3xl font-bold text-center mb-8 ${darkMode ? 'text-white' : 'text-gray-800'}`}>Skills</h2>
-                <div className="grid grid-cols-3 md:grid-cols-3 lg:grid-cols-6 gap-6">
-                    {skills.map((skill, index) => (
+                <div className="flex flex-wrap justify-center md:space-x-6 sm:xs-space-1 xs:space-x-2">
+                    {programmingLanguages.map((skill, index) => (
+                        <SkillCard key={index} skill={skill.skill} Icon={skill.icon} color={skill.color} />
+                    ))}
+                </div>
+                <div className="flex flex-wrap justify-center md:space-x-6 sm:xs-space-1 xs:space-x-2">
+                    {frameworks.map((skill, index) => (
+                        <SkillCard key={index} skill={skill.skill} Icon={skill.icon} color={skill.color} />
+                    ))}
+                </div>
+                <div className="flex flex-wrap justify-center md:space-x-6 sm:xs-space-1 xs:space-x-2">
+                    {tools.map((skill, index) => (
                         <SkillCard key={index} skill={skill.skill} Icon={skill.icon} color={skill.color} />
                     ))}
                 </div>
